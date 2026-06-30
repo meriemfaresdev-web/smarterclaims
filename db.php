77ec9cae9843ccb495672e5adae4a21e3f9b9685
meriem fared <meriemfares1710@gmail.com>
2026-06-30 02:45:14 +0100

    Fix syntax error in db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
 // حطي القيم الحقيقية اللي نقلتيها من Railway ديريكت هنا:
-$host   = 'mysql.railway.internal'; // حطي الـ SQL_HOST ديالك هنا
-$port   = '3306';                  // الـ SQL_PORT
-$dbname = 'railway';               // حطي هنا السميّة الحقيقية اللي لقيتيها فـ SQL_DATABASE (غالباً railway)
-$user   = 'root';                  // الـ SQL_USER
-$pass   = 'KHSoFOeqdqdYdpXFvzGXqFKwTyWRNSSV'; // حطي الـ SQL_PASSWORD الحقيقي هنا فاش تفتحيه
+$host   = 'mysql.railway.internal'; 
+$port   = '3306';                  
+$dbname = 'railway';                // تأكدي غي واش سميتها railway ف السيرفر أو سمية أخرى
+$user   = 'root';                  
+$pass   = 'KHSoFOeqdqdYdpXFvzGXqFKwTyWRNSSV'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion : " . " . $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
